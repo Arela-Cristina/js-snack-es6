3867d6bd797c178ea4cf82ec360e1ab3fd60f13b
Arela-Cristina <instagrandine@gmail.com>
2024-10-25 16:42:08 +0200

    create an object array

--- snack2/snack2.js
+++ snack2/snack2.js
@@ -2,5 +2,40 @@
 // Creare un array di oggetti di squadre di calcio.
 //  Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti.
 // Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
+const nationalTeams = [
+    {
+        name: "Italy",
+        pointsScored: 0,
+        pointsMissed: 0
+    },
+    {
+        name: "Argentina",
+        pointsScored: 0,
+        pointsMissed: 0
+    },
+    {
+        name: "Germany",
+        pointsScored: 0,
+        pointsMissed: 0
+    },
+    {
+        name: "France",
+        pointsScored: 0,
+        pointsMissed: 0
+    },
+    {
+        name: "Peru",
+        pointsScored: 0,
+        pointsMissed: 0
+    }
+];
+
+
+
+
+
+
+
 // Generare numeri random al posto degli 0 nelle proprietà “punti” fatti e “falli subiti”.
 // Infine, creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
+
