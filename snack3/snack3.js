37a7a43c14dda686e78795a949b2303284f91017
Arela-Cristina <instagrandine@gmail.com>
2024-10-25 17:22:54 +0200

    declarate a function

---
+++ snack3/snack3.js
@@ -0,0 +1,9 @@
+// Snack 3 (Bonus)
+// Scrivere una funzione che accetti tre argomenti, un array e due numeri (a più piccolo di b).
+// La funzione ritornerà un nuovo array con i valori che hanno la posizione compresa tra i due numeri.
+
+
+function myFunction(array, a, b) {
+    // ritornerà un nuovo array
+
+}
