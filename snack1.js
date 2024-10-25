e69ca745e1c027b1e36780b3e24f5616c1eb33b3
Arela-Cristina <instagrandine@gmail.com>
2024-10-25 16:14:02 +0200

    create an array

--- snack1.js
+++ snack1.js
@@ -2,3 +2,22 @@
 // Creare un array di oggetti:
 // Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
 // Stampare a schermo la bici con peso minore.
+
+const bike = [
+    {
+        name: "mountainBike",
+        weight: 14.5
+    },
+    {
+        name: "roadBike",
+        weight: 8.3
+    },
+    {
+        name: "bMXbike",
+        weight: 10.2
+    },
+    {
+        name: "foldingBike",
+        weight: 12.1
+    }
+];
